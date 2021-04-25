d79d885f386706464910ac149a2c99a34a731bef
Zion <ekozmos25@gmail.com>
2021-04-25 06:19:55 -0400

    More WIP

--- Assets/Scenes/ItemPickup.cs
+++ Assets/Scenes/ItemPickup.cs
@@ -9,14 +9,14 @@ public class ItemPickup : MonoBehaviour
     public void PlayerInteract()
     {
 
-      //  Inventory.main.hasKey = true;
-      //  Inventory.main.hasBooks = true;
-       // Inventory.main.hasLaptop = true;
-      //  Inventory.main.hasMoney = true;
-       // Inventory.main.hasPhone1 = true;
-       //Inventory.main.hasPhone2 = true;
-        //Inventory.main.hasHat = true;
-        Inventory.main.hasBall = true;
+       Inventory.main.hasKey = true;
+       Inventory.main.hasBooks = true;
+       Inventory.main.hasLaptop = true;
+       Inventory.main.hasMoney = true;
+       Inventory.main.hasPhone1 = true;
+       Inventory.main.hasPhone2 = true;
+       Inventory.main.hasHat = true;
+       Inventory.main.hasBall = true;
 
 
         Destroy(gameObject);
